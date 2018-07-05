d6e75228a1aa6c24e0c4a7c206fe7693069c42ef
cr4ftsm4n <35619764+cr4ftsm4n@users.noreply.github.com>
2018-07-05 22:48:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 from urllib.parse import parse_qs
+import shutil
+
 
 VID = "tk2AXB3wf9s"
 
@@ -9,12 +11,16 @@ def parse(qs):
     stream_list = d["url_encoded_fmt_stream_map"][0].split(',')
     d2 = parse_qs(stream_list[0])
     url = d2["url"]
-    print(url[0])
+    return url
 
 
 def main():
     resp = requests.get("https://www.youtube.com/get_video_info?video_id={}".format(VID))
-    parse(resp.text)
+    url = parse(resp.text)[0]
+    res = requests.get(url)  # video url
+    with open("{}.mp4".format(VID), 'wb') as file:
+        # res.raw.decode_content = True
+        shutil.copyfileobj(res.raw, file)
 
 
 if __name__ == "__main__":
