500b9c08a6bb2414fc2253503686b47a5c1d2e74
cr4ftsm4n <35619764+cr4ftsm4n@users.noreply.github.com>
2018-07-05 22:29:36 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("nihao")
+import requests
+from urllib.parse import parse_qs
+
+VID = "tk2AXB3wf9s"
+
+
+def parse(qs):
+    d = parse_qs(qs)
+    m = d["url_encoded_fmt_stream_map"][0]
+    d2 = parse_qs(m)
+    url = d2["url"]
+    print(url[0])
+
+
+def main():
+    resp = requests.get("https://www.youtube.com/get_video_info?video_id={}".format(VID))
+    parse(resp.text)
+
+
+if __name__ == "__main__":
+    main()
