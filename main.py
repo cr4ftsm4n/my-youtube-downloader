d9f9025a0c91643081fa14162bdaf6f956381cad
cr4ftsm4n <35619764+cr4ftsm4n@users.noreply.github.com>
2018-07-12 21:52:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from urllib.parse import parse_qs
 import shutil
 
 
-VID = "AQsAuvGIP9U"
+# VID = "AQsAuvGIP9U"
 
 
 def parse(qs):
@@ -23,16 +23,18 @@ def main():
     }
     resp = requests.get(
         "https://www.googleapis.com/youtube/v3/playlistItems", params=params)
-    print(resp.text)
+    for item in resp.json()["items"]:
+        id = item["contentDetails"]["videoId"]
+        download_video(id)
 
 
-def download_video():
+def download_video(id):
     resp = requests.get(
-        "https://www.youtube.com/get_video_info?video_id={}".format(VID))
+        "https://www.youtube.com/get_video_info?video_id={}".format(id))
     url = parse(resp.text)[0]
     res = requests.get(url, stream=True)  # video url
     res.raw.decode_content = True
-    with open("{}.mp4".format(VID), 'wb') as f:
+    with open("{}.mp4".format(id), 'wb') as f:
         shutil.copyfileobj(res.raw, f)
 
 
