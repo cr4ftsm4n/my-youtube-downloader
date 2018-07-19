be4eba1c57673394a90ce2219f84b88f138159fa
cr4ftsm4n <35619764+cr4ftsm4n@users.noreply.github.com>
2018-07-20 00:30:50 +0900

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -12,7 +12,7 @@ python main.py
 git add latest_id
 git status
 git -c "commit.gpgsign=false" \
-    -c "user.name=${GIT_NAME}" \
-    -c "user.email=${GIT_EMAIL}" \
-    commit -m "${GIT_COMMIT_MESSAGE}"
+    -c "user.name=${GIT_NAME}" \
+    -c "user.email=${GIT_EMAIL}" \
+    commit -m "${GIT_COMMIT_MESSAGE}"
 git push "https://x-token:${GITHUB_TOKEN}@github.com/${GIT_REPO}" "${GIT_BRANCH}"
